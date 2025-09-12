82aada33ef1a64217652e7a147d73f5b4e605b74
talaat <talaatahmed.806@gmail.com>
2025-09-12 10:51:29 +0300

    fix: remove keyword new from Router

--- src/Modules/Welcome/welcome.controller.js
+++ src/Modules/Welcome/welcome.controller.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { WelcomeService } from "./Service/welcome.service";
+import { WelcomeService } from "./Service/welcome.service.js";
 
 const router = Router();
 
